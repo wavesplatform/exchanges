aeae91792fcf9c7431ff3e5922264db1ef192e33
Alex Kordys <akordys@web3tech.ru>
2023-09-25 19:12:24 +0300

    Missing down script

--- crates/database/migrations/2023-08-09-103152_daily_aggregates/down.sql
+++ crates/database/migrations/2023-08-09-103152_daily_aggregates/down.sql
@@ -1,1 +1,6 @@
 DROP TABLE exchange_transactions_daily_price_aggregates;
+
+DROP AGGREGATE public.first (anyelement);
+DROP FUNCTION public.first_agg;
+DROP AGGREGATE public.last (anyelement);
+DROP FUNCTION public.last_agg;
